1c2495b16b028c87e7147464a5f307ec652e82bd
Altar327 <altar327@gmail.com>
2021-05-27 04:26:38 +0700

    Prog_with_rev

--- Programm_5_1_2/main.c
+++ Programm_5_1_2/main.c
@@ -8,11 +8,12 @@ typedef struct Node_ {          //Вершины графа
 } Node;
 
 
-typedef struct list_ {               //Список смежности
+typedef struct List_ {               //Список смежности
     int value;
+    int flag;
     int name_of_the_next_node;
-    struct list_ *child;
-} list;
+    struct List_ *child;
+} List;
 
 
 void swap_node (Node *a, Node *b) {
@@ -21,26 +22,26 @@ void swap_node (Node *a, Node *b) {
     *b = c;
 }
 
-list* del_from_list_first_value (list *head, int value) {
-    list *prev = NULL;
-    list *curr = head;
-    while (curr) {
-        if (curr->value == value) {
-            if (prev) {
-                prev->child = curr->child;
-                free(curr);
-                return head;
-            } else {
-                head = curr->child;
-//                curr->child = NULL;
-                free(curr);
-                return head;
-            }
-        }
-        prev = curr;
-        curr = curr->child;
-    }
-}
+//list* del_from_list_first_value (list *head, int value) {
+//    list *prev = NULL;
+//    list *curr = head;
+//    while (curr) {
+//        if (curr->value == value) {
+//            if (prev) {
+//                prev->child = curr->child;
+//                free(curr);
+//                return head;
+//            } else {
+//                head = curr->child;
+////                curr->child = NULL;
+//                free(curr);
+//                return head;
+//            }
+//        }
+//        prev = curr;
+//        curr = curr->child;
+//    }
+//}
 
 void sift_down (int i, Node *queue, int queue_size) {                //Просеивание вниз
     while (2 * i + 1 < queue_size) {
@@ -80,11 +81,23 @@ Node* insert_in_queue (Node new_node, Node* queue, int *queue_size) {
     return queue;
 }
 
+void go (int node, int* array_prev_node_for_i_node) {
+    if (array_prev_node_for_i_node[node] != -1) {
+        go(array_prev_node_for_i_node[node], array_prev_node_for_i_node);
+        printf("%d ", node);
+    } else {
+        printf("0 ");
+    }
+}
+
 int main() {
     int final_node, count_node, count_edge;
     scanf("%d %d %d", &final_node, &count_node, &count_edge);
 
-    list **lists_adjacency = (list**)malloc(count_node * sizeof(list*));           //массив листов смежности
+    List **lists_adjacency = (List**)malloc(count_node * sizeof(List*));           //массив листов смежности
+    if (lists_adjacency == NULL) {
+        return 0;
+    }
     for (int i = 0; i < count_node; i++) {
         lists_adjacency[i] = NULL;
     }
@@ -93,94 +106,135 @@ int main() {
     for (int i = 0; i < count_edge; i++) {
         scanf("%d %d %d", &x, &y, &way);
         if (lists_adjacency[x] != NULL) {
-            list* list_for_search = lists_adjacency[x];
+            List* list_for_search = lists_adjacency[x];
             while (list_for_search->child != NULL) {
                 list_for_search = list_for_search->child;
             }
-            list_for_search->child = (list *) malloc(sizeof(list));
+            list_for_search->child = (List *) malloc(sizeof(List));
+            if (list_for_search->child == NULL) {
+                return 0;
+            }
             if (list_for_search->child) {
                 list_for_search->child->name_of_the_next_node = y;
                 list_for_search->child->value = way;
+                list_for_search->child->flag = 0;
                 list_for_search->child->child = NULL;
             }
         } else {
-            lists_adjacency[x] = (list *) malloc(sizeof(list));
+            lists_adjacency[x] = (List *) malloc(sizeof(List));
+            if (lists_adjacency[x] == NULL) {
+                return 0;
+            }
             lists_adjacency[x]->name_of_the_next_node = y;
             lists_adjacency[x]->value = way;
+            lists_adjacency[x]->flag = 0;
             lists_adjacency[x]->child = NULL;
         }
     }
 
     int* keys = (int*)calloc(count_node, sizeof(int));
+    if (keys == NULL) {
+        return 0;
+    }
     //Массив с текущим "значением дастяжения вершин" (назаву так длину пути до i вершины, где i место ключа в массиве)
     //Будем сичтать, что вершина не открыта если еее ключ равен 0, за исключением стартовой.
 
-    int **array_ways_to_the_nodes = (int**)malloc(count_node * sizeof(int*));
-    for (int i = 0; i < count_node; i++) {
-        array_ways_to_the_nodes[i] = (int *)calloc(count_node + 1, sizeof(int));
+//    int **array_ways_to_the_nodes = (int**)malloc(count_node * sizeof(int*));
+//    for (int i = 0; i < count_node; i++) {
+//        array_ways_to_the_nodes[i] = (int *)calloc(count_node + 1, sizeof(int));
+//    }
+    int *array_prev_node_for_i_node = (int*) calloc(count_node, sizeof(int));
+    if (array_prev_node_for_i_node == NULL) {
+        return 0;
     }
+    array_prev_node_for_i_node[start_node] = -1;
+
     //Массив, внутри которого массивы описывающие путь до вершины i, при этом условимся что на 0 месте будет стоять кол-во элементов в этом массиве
 
     Node *queue = (Node *)malloc(count_node * sizeof(Node));               //Куча вершин
+    if (queue == NULL) {
+        return 0;
+    }
     int queue_size = 1;                                             //Кол-во элементов в очереди
 
     Node first_node;
     first_node.name = start_node;
     first_node.key = 0;
-    array_ways_to_the_nodes[first_node.name][0] = 1;
-    array_ways_to_the_nodes[first_node.name][1] = 0;
+//    array_ways_to_the_nodes[first_node.name][0] = 1;
+//    array_ways_to_the_nodes[first_node.name][1] = 0;
     keys[start_node] = 0;
 
     *queue = first_node;
 
     while (queue_size) {
         Node *curr_node = queue;
-        if (lists_adjacency[curr_node->name] == NULL) {
+
+        int flag = 0;
+//        int curr_value = lists_adjacency[curr_node->name]->value;
+//        int curr_name = lists_adjacency[curr_node->name]->name_of_the_next_node;
+        int curr_value;
+        int curr_name;
+
+        List *list_for_search = lists_adjacency[curr_node->name];                //Переменная чтобы идти по списку смежности
+        while (list_for_search != NULL) {
+            if (list_for_search->flag == 0) {
+                curr_value = list_for_search->value;
+                curr_name = list_for_search->name_of_the_next_node;
+                list_for_search->flag++;
+                flag++;
+                break;
+            }
+            list_for_search = list_for_search->child;
+        }
+
+        if (!flag) {
             queue = remove_from_queue(queue, &queue_size);
         } else if (curr_node) {
 
-            int min_value = lists_adjacency[curr_node->name]->value;        //Переменные для поиска минимального пути из вершины
-            int min_name = lists_adjacency[curr_node->name]->name_of_the_next_node;
+//            list *list_for_search = lists_adjacency[curr_node->name]->child;                //Переменная чтобы идти по списку смежности
+//            while (list_for_search) {
+//                if (!list_for_search->flag) {
+//                    curr_value = list_for_search->value;
+//                    curr_name = list_for_search->name_of_the_next_node;
+//                    list_for_search->flag++;
+//                    break;
+//                }
+//                list_for_search = list_for_search->child;
+//            }
 
-
-            list *list_for_search = lists_adjacency[curr_node->name]->child;                //Переменная чтобы идти по списку смежности
-            while (list_for_search) {               //Ищем минимальный элемент, проходя по всему списку
-                if (min_value > list_for_search->value) {
-                    min_value = list_for_search->value;
-                    min_name = list_for_search->name_of_the_next_node;
-                }
-                list_for_search = list_for_search->child;
-            }
-
-            if (keys[min_name] > min_value + curr_node->key) {           //Открываем или переоткрываем вершину
-                keys[min_name] = min_value + curr_node->key;
+            if (keys[curr_name] > curr_value + curr_node->key) {           //Открываем или переоткрываем вершину
+                keys[curr_name] = curr_value + curr_node->key;
                 Node *new_node = queue;
-                while (new_node->name != min_name) {
+                while (new_node->name != curr_name) {
                     new_node++;
                 }
-                new_node->key = keys[min_name];
-                for (int i = 0; i <= array_ways_to_the_nodes[curr_node->name][0]; i++) {
-                    array_ways_to_the_nodes[min_name][i] = array_ways_to_the_nodes[curr_node->name][i];
-                }
-                array_ways_to_the_nodes[min_name][0] += 1;
-                array_ways_to_the_nodes[min_name][array_ways_to_the_nodes[min_name][0]] = min_name;
+                new_node->key = keys[curr_name];
+//                for (int i = 0; i <= array_ways_to_the_nodes[curr_node->name][0]; i++) {
+//                    array_ways_to_the_nodes[curr_name][i] = array_ways_to_the_nodes[curr_node->name][i];
+//                }
+//                array_ways_to_the_nodes[curr_name][0] += 1;
+//                array_ways_to_the_nodes[curr_name][array_ways_to_the_nodes[curr_name][0]] = curr_name;
+                array_prev_node_for_i_node[curr_name] = curr_node->name;
 
-            } else if (keys[min_name] == 0 && min_name != 0) {
-                keys[min_name] = min_value + curr_node->key;
+            } else if (keys[curr_name] == 0 && curr_name != 0) {
+                keys[curr_name] = curr_value + curr_node->key;
 
-                for (int i = 0; i <= array_ways_to_the_nodes[curr_node->name][0]; i++) {
-                    array_ways_to_the_nodes[min_name][i] = array_ways_to_the_nodes[curr_node->name][i];
-                }
-                array_ways_to_the_nodes[min_name][0] += 1;
-                array_ways_to_the_nodes[min_name][array_ways_to_the_nodes[min_name][0]] = min_name;
+//                for (int i = 0; i <= array_ways_to_the_nodes[curr_node->name][0]; i++) {
+//                    array_ways_to_the_nodes[curr_name][i] = array_ways_to_the_nodes[curr_node->name][i];
+//                }
+//                array_ways_to_the_nodes[curr_name][0] += 1;
+//                array_ways_to_the_nodes[curr_name][array_ways_to_the_nodes[curr_name][0]] = curr_name;
+
+                array_prev_node_for_i_node[curr_name] = curr_node->name;
+
 
                 Node new_node;
-                new_node.key = keys[min_name];
-                new_node.name = min_name;
+                new_node.key = keys[curr_name];
+                new_node.name = curr_name;
 
                 queue = insert_in_queue(new_node, queue, &queue_size);
             }
-            lists_adjacency[curr_node->name] = del_from_list_first_value(lists_adjacency[curr_node->name], min_value);         //удаляем путь по которому прошлись
+//            lists_adjacency[curr_node->name] = del_from_list_first_value(lists_adjacency[curr_node->name], curr_value);         //удаляем путь по которому прошлись
         }
     }
 
@@ -189,15 +243,13 @@ int main() {
 //    }
 
     printf("%d\n", keys[final_node]);
-    for (int i = 1; i <= array_ways_to_the_nodes[final_node][0]; i++) {
-        printf("%d ", array_ways_to_the_nodes[final_node][i]);
-    }
+    go(final_node, array_prev_node_for_i_node);
 
     for (int i = 0; i < count_node; i++) {
         free(lists_adjacency[i]);
-        free(array_ways_to_the_nodes[i]);
+//        free(array_prev_node_for_i_node[i]);
     }
-    free(array_ways_to_the_nodes);
+    free(array_prev_node_for_i_node);
     free(lists_adjacency);
     free(queue);
 
